d67bf1a62774f3578c5906b15c02ab9edc22e9fa
Paolo96700 <paolo.falcoapp@gmail.com>
2023-07-20 14:28:51 +0200

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,37 +10,37 @@ export default {
 </script>
 
 <template>
-    <div class="big_container">
-        <div class="c_ontainer">
-            <button class="mybtn" type="button" data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasWithBothOptions"></button>
+    <div>
+        <div class="big_container">
+            <div class="c_ontainer">
+                <button class="mybtn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions">
+                </button>
+            </div>
+        </div>
+
+        <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
+            aria-labelledby="offcanvasWithBothOptionsLabel">
+            <ul>
+                <li>
+                    <router-link :to="{ name: 'home' }" class="nav-link active" aria-current="page">
+                        Home
+                    </router-link>
+                </li>
+                <li>
+                    <router-link :to="{ name: 'about-us' }" class="nav-link active" aria-current="page">
+                        About
+                    </router-link>
+                </li>
+                <li>
+                    <router-link :to="{ name: 'contacts' }" class="nav-link active" aria-current="page">
+                        Contacs
+                    </router-link>
+                </li>
+            </ul>
+
         </div>
     </div>
-
-    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
-        aria-labelledby="offcanvasWithBothOptionsLabel">
-        <ul>
-            <li>
-                <router-link :to="{ name: 'home' }" class="nav-link active" aria-current="page">
-                    Home
-                </router-link>
-            </li>
-            <li>
-                <router-link :to="{ name: 'about-us' }" class="nav-link active" aria-current="page">
-                    About
-                </router-link>
-            </li>
-            <li>
-                <router-link :to="{ name: 'contacts' }" class="nav-link active" aria-current="page">
-                    Contacs
-                </router-link>
-            </li>
-        </ul>
-
-    </div>
-    </div>
-
-</div></template>
+</template>
 
 
 <style lang="scss" scoped>
