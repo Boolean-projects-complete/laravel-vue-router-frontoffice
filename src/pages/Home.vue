9c715d99c19a4f6fad96fcb60ae3b6a7ec805d5a
GianluGiarde <gianlugiarde@gmail.com>
2023-07-20 19:33:25 +0200

    add api

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,32 +1,59 @@
 <script>
-import DrinkFilter from '../components/DrinkFilter.vue';
-import DrinkCard from '../components/DrinkCard.vue'
-
+import DrinkFilter from "../components/DrinkFilter.vue";
+import DrinkCard from "../components/DrinkCard.vue";
+import axios from "axios";
 
 export default {
-    components: {
-        DrinkFilter, DrinkCard,
-    }
+  components: {
+    DrinkFilter,
+    DrinkCard,
+  },
+  data() {
+    return {
+      arrCocktails: [],
+    };
+  },
+  methods: {
+    getCocktails() {
+      axios.get("http://localhost:8000/api/cocktails").then((response) => {
+        this.arrCocktails = response.data.data;
+      });
+    },
+  },
+  created() {
+    this.getCocktails();
+  },
 };
 </script>
 
 <template>
-    <div class="d-flex flex-column align-items-center">
-        <DrinkFilter class="filter" />
-        <div class="big_container_cards">
-            <DrinkCard />
-        </div>
-    </div>
-</template>
+  <div class="d-flex flex-column align-items-center">
+    <DrinkFilter class="filter" />
 
+    <div class="big_container_cards">
+      <div class="container_card d-flex justify-content-center">
+        <DrinkCard
+          v-for="cocktail in arrCocktails"
+          :key="cocktail.id"
+          :objCocktail="cocktail"
+        />
+      </div>
+    </div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 .filter {
-    width: 100%;
+  width: 100%;
 }
 
 .big_container_cards {
-    max-width: 1200px;
-    margin-top: 5rem;
+  max-width: 1200px;
+  margin-top: 5rem;
+}
+
+.container_card {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
