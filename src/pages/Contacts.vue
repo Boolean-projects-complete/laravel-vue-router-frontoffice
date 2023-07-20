bf768e5f55980cf2c1d8b80f4a9cad96b987ff76
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-07-20 17:25:30 +0200

    end

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -4,9 +4,40 @@
 
 <template>
     <div>
-        <h1>Contacts</h1>
+        <h1>Contattaci</h1>
+        <form action="">
+            <label for="name">Nome</label>
+            <input class="inpt" type="text" id="name">
+            <label for="surname">Cognome</label>
+            <input class="inpt" type="text" id="surname">
+            <label for="email">Email</label>
+            <input class="inpt" type="email" id="email">
+            <label for="message">Corpo del messaggio</label>
+            <textarea class="inpt" name="message" id="message" cols="30" rows="10"></textarea>
+        </form>
     </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../assets/styles/general.scss' as *;
+    
+div{
+        max-width: 70%;
+        margin: 0 auto;
+        padding: 2rem;
+        background-color: $c-bg;
+        color: white;
+
+        form {
+            @include dfj;
+            flex-direction: column;
+            padding: 2rem;
+            .inpt{
+                background-color: rgba(242, 242, 242, 0);
+                border: 2px solid $c-bg-txt;
+                border-radius: 3px;
+            }
+    }
+    }
+</style>
