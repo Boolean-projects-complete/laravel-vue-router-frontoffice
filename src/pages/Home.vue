1bbfbc2658c9d58032633449ac045893af5e8e76
GianluGiarde <gianlugiarde@gmail.com>
2023-07-21 04:05:18 +0200

    add category & search

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,24 +11,43 @@ export default {
     data() {
         return {
             arrCocktails: [],
+            arrCategory: [],
+            searchString: "",
+            category: "",
         };
     },
     methods: {
         getCocktails() {
-            axios.get("http://localhost:8000/api/cocktails").then((response) => {
+            axios.get("http://localhost:8000/api/cocktails", {
+                params: {
+                    q: this.searchString,
+                    category: this.category,
+                }
+            }).then(response => {
                 this.arrCocktails = response.data.data;
             });
         },
+        getCategory() {
+            axios.get("http://localhost:8000/api/category").then(response => {
+                this.arrCategory = response.data;
+            });
+        },
+        startSearch({ category, searchString }) {
+            this.category = category
+            this.searchString = searchString
+            this.getCocktails()
+        }
     },
     created() {
         this.getCocktails();
+        this.getCategory();
     },
 };
 </script>
 
 <template>
     <div class="d-flex flex-column align-items-center">
-        <DrinkFilter class="filter" />
+        <DrinkFilter class="filter" :arrCategory="arrCategory" @pressBtn="startSearch($event)" />
 
         <div class="big_container_cards">
             <div class="container_card d-flex justify-content-center">
